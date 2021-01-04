b5b25487c92879406d2a22bed1b2ff2571485022
KanuKim97 <KanuKim97@users.noreply.github.com>
2021-01-04 23:18:59 +0900

    Update Image_Classification_v2.py

--- Image_Adv/Image_Classification_v2.py
+++ Image_Adv/Image_Classification_v2.py
@@ -57,8 +57,62 @@ for image_batch, labels_batch in train_ds:
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
-train_ds = train_ds.cache().shuffle(1000).prefetch(bufefr_size = AUTOTUNE)
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size = AUTOTUNE)
 val_ds = val_ds.prefetch(buffer_size=AUTOTUNE)
 
 nomalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
+nomalized_ds = train_ds.map(lambda x, y: (nomalization_layer(x), y))
+image_batch, labels_batch = next(iter(nomalized_ds))
+first_img = image_batch[0]
+print(np.min(first_img), np.max(first_img))
 
+num_classes = 5
+
+model = Sequential([
+    layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+    layers.Conv2D(16, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(num_classes)
+])
+
+model.compile(
+    optimizer='adam',
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy']
+)
+
+model.summary()
+
+history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=10
+)
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
