88fde0a5053428080d957fc87b55055a398584e0
KanuKim97 <KanuKim97@users.noreply.github.com>
2021-02-16 18:31:54 +0900

    Update Neural_Style_Transger.py
    Update code

--- Neural_Style_Transger.py
+++ Neural_Style_Transger.py
@@ -59,4 +59,95 @@ hub_model = tf_hub.load('https://tfhub.dev/google/magenta/arbitrary-image-styliz
 stylized_image = hub_model(tf.constant(content_image), tf.constant(style_image))[0]
 tensor_to_image(stylized_image)
 
-x = tf.keras.applicationsl.vgg19.preprocess_input(content_image*255)
+x = tf.keras.applications.vgg19.preprocess_input(content_image*255)
+x = tf.image.resize(x, (224, 224))
+vgg = tf.keras.applications.VGG19(include_top=True, weights='imagenet')
+prediction_probabilities = vgg(x)
+prediction_probabilities.shape
+
+predicted_top_5 = tf.keras.applications.vgg19.decode_predictions(prediction_probabilities.numpy())[0]
+[(class_name, prob) for (number, class_name, prob) in predicted_top_5]
+
+vgg = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
+
+print()
+for layer in vgg.layers:
+    print(layer.name)
+
+content_layers = ['block5_conv2']
+style_layers = ['block1_conv1',
+                'block2_conv1',
+                'block3_conv1',
+                'block4_conv1',
+                'block5_conv1']
+num_content_layers = len(content_layers)
+num_style_layers = len(style_layers)
+
+def vgg_layers(layer_names):
+    vgg = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
+    vgg.trainable = False
+
+    outputs = [vgg.get_layer(name).output for name in layer_names]
+
+    model = tf.keras.Model([vgg.input], outputs)
+    return model
+
+style_extractor = vgg_layers(style_layers)
+style_outputs = style_extractor(style_image*255)
+
+for name, output in zip(style_layers, style_outputs):
+    print(name)
+    print(" 크기 : ", output.numpy().shape)
+    print(" 최솟값 : ", output.numpy().min())
+    print(" 최대값 : ", output.numpy().max())
+    print(" 평균 : ", output.numpy().mean())
+    print()
+
+def gram_matrix(input_tensor):
+    result = tf.linalg.einsum('bijc,bijd->bcd', input_tensor, input_tensor)
+    input_shape = tf.shape(input_tensor)
+    num_locations = tf.cast(input_shape[1] * input_shape[2], tf.float32)
+    return result/(num_locations)
+
+class StyleContentModel(tf.keras.models.Model):
+    def __init__(self, style_layers, content_layers):
+        super(StyleContentModel, self).__init__()
+        self.vgg = vgg_layers(style_layers + content_layers)
+        self.style_layers = style_layers
+        self.content_layers = content_layers
+        self.num_style_layers = len(style_layers)
+        self.vgg.trainable = False
+
+    def call(self, inputs):
+        inputs = inputs*255.0
+        preprocessed_input = tf.keras.applications.vgg19.preprocess_input(inputs)
+        outputs = self.vgg(preprocessed_input)
+        style_outputs, content_outputs = (outputs[:self.num_style_layers], outputs[self.num_style_layers])
+        style_outputs = [gram_matrix(style_output) for style_output in style_outputs]
+        content_dict = {content_name:value for content_name, value in zip(self.content_layers, content_outputs)}
+        style_dict = {style_name:value for style_name, value in zip(self.style_layers, style_outputs)}
+        return {'content':content_dict, 'style':style_dict}
+
+extractor = StyleContentModel(style_layers, content_layers)
+results = extractor(tf.constant(content_image))
+
+print('스타일')
+for name, output in sorted(results['style'].items()):
+    print(" ", name)
+    print("     크기 : ", output.numpy().shape)
+    print("     최솟값 : ", output.numpy().min())
+    print("     최댓값 : ", output.numpy().max())
+    print("     ;평균 : ", output.numpy().mean())
+    print()
+
+print("콘텐츠: ")
+for name, output in sorted(results['content'].items()):
+    print(" ", name)
+    print("     크기 : ", output.numpy().shape)
+    print("     최솟값 : ", output.numpy().min())
+    print("     최댓값 : ", output.numpy().max())
+    print("     ;평균 : ", output.numpy().mean())
+    print()
+
+
+   
