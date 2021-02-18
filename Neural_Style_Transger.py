22fc2c0b07ff8a68fb1f0e7da300bc05f1425729
KanuKim97 <KanuKim97@users.noreply.github.com>
2021-02-18 22:56:45 +0900

    Update_NST
    TypeError: zip argument #2 must support iteration

--- Neural_Style_Transger.py
+++ Neural_Style_Transger.py
@@ -149,5 +149,42 @@ for name, output in sorted(results['content'].items()):
     print("     ;평균 : ", output.numpy().mean())
     print()
 
+sytle_targets = extractor(style_image)['style']
+content_targets = extractor(content_image)['content']
+image = tf.Variable(content_image)
 
-   
+def clip_0_1(image):
+    return tf.clip_by_value(image, clip_value_min=0.0, clip_value_max=1.0)
+
+opt = tf.optimizers.Adam(learning_rate=0.02, beta_1=0.99, epsilon=1e-1)
+
+style_weight = 1e-2
+conetnt_weight = 1e4
+
+def style_content_loss(outputs): 
+    style_outputs = outputs['style']
+    content_outputs = outputs['content']
+    style_loss = tf.add_n([tf.reduce_mean((style_outputs[name]-sytle_targets[name])**2)
+                            for name in style_outputs.keys()])
+    style_loss *= style_weight / num_style_layers
+
+    content_loss = tf.add_n([tf.reduce_mean((content_outputs[name]-sytle_targets[name])**2)
+                            for name in content_outputs.keys()])
+    loss = content_loss + style_loss
+    return loss
+
+@tf.function()
+def train_step(image):
+  with tf.GradientTape() as tape:
+    outputs = extractor(image)
+    loss = style_content_loss(outputs)
+
+  grad = tape.gradient(loss, image)
+  opt.apply_gradients([(grad, image)])
+  image.assign(clip_0_1(image))
+
+
+train_step(image)
+train_step(image)
+train_step(image)
+tensor_to_image(image)
